88fc80612b3d2f648f1f2fde2efdbf0de501e2e8
Iryna Markovich <i.markovich@godeltech.com>
2018-10-29 15:27:01 +0300

    clear

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Root from "./components/Root";
-import configureStore from "./configureStore";
-
-const store = configureStore();
-
-render(<Root store={store} />, document.getElementById("root"));
